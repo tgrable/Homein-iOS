ade0096e778599d45f44caaf308011b5005404b5
Tim Grable <tgrable04@yahoo.com>
2015-10-28 08:36:48 -0500

    Initial Commit

---
+++ FirstMortgage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstMortgage
+//
+//  Created by Timothy C Grable on 10/28/15.
+//  Copyright © 2015 Trekk Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
