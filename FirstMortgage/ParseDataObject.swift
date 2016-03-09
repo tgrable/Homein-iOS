a24326ff3de7d2c77209d80998d7c4619724e841
Justin Davis <jjustind@gmail.com>
2016-03-09 13:41:35 -0600

    •merged with Fabric branch - Master branch is now fully Fabric and Crashlytics enabled.

--- FirstMortgage/ParseDataObject.swift
+++ FirstMortgage/ParseDataObject.swift
@@ -120,8 +120,6 @@ class ParseDataObject: NSObject {
     }
     
     func emailLoanOfficer(name: String, email: String, loanOfficer: String) {
-        
-
 
         PFCloud.callFunctionInBackground("loanOfficer", withParameters: ["name" : name, "email": email, "officer" : loanOfficer]) { (result: AnyObject?, error: NSError?) in
             print("----- Email LO -----")
